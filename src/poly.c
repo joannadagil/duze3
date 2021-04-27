83a9d29c7d7dc96add74bd8747f5ab27d5832311
JoannaDagil <jd417531@students.mimuw.edu.pl>
2021-04-27 14:40:34 +0200

    poprawiony PolyAdd

--- src/poly.c
+++ src/poly.c
@@ -1,14 +1,13 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 #include "poly.h"
 
-#define STARTING_ARRAY 100
-
 void PolyDestroy(Poly *p){
   if(p->arr){
-    for(int i = 0; i < p->size; i++){
+    for(size_t i = 0; i < p->size; i++){
       MonoDestroy(&((p->arr)[i]));
     }
   }
@@ -19,22 +18,31 @@ Poly PolyClone(const Poly *p){
   if(p->arr){
     clone.size = p->size;
     clone.arr = malloc(p->size * sizeof(struct Mono));
-    for(int i = 0; i < p->size; i++){
+    for(size_t i = 0; i < p->size; i++){
       (clone.arr)[i] = MonoClone(&((p->arr)[i]));
     }
-  }else
+  }else{
     clone.coeff = p->coeff;
+    clone.arr = NULL;
+  }
   return clone;
 }
 
+
+/**
+ * Dodaje dwa wielomiany, z których conajmniej jeden jest wielomianem prostym.
+ * @param[in] p : wielomian @f$p@f$
+ * @param[in] q : wielomian @f$q@f$
+ * @return @f$p + q@f$
+ */
 static inline Poly AddUnproperPolys(const Poly *p, const Poly *q){
   //assert(!p->arr || !q->arr);
   if(!p->arr && !q->arr)
     return PolyFromCoeff(p->coeff + q->coeff);
   if(!p->arr){
     Poly temp = (Poly) {.size = 1, .arr = malloc(sizeof(Mono))};
-    (temp.arr)[0] = MonoFromPoly(p->coeff, 0);
-    Poly sum = PolyAdd(q, &temp);
+    (temp.arr)[0] = MonoFromPoly(p, 0);
+    Poly sum = PolyAdd(&temp, q);
     PolyDestroy(&temp);
     return sum;
   }    
@@ -62,19 +70,47 @@ Poly PolyAdd(const Poly *p, const Poly *q){
     }
   }
   sum.arr = realloc(sum.arr, isum * sizeof(struct Mono));
-  sum.size = isum;
+  sum.size = isum; //o dziwo działa też dla zerujących się wielomianów
   return sum;
 }
 
+static inline void MonosSwap(Mono *a, Mono *b){
+  Mono temp = *a;
+  *a = *b;
+  *b = temp;
+}
 
-/**
- * Sumuje listę jednomianów i tworzy z nich wielomian.
- * Przejmuje na własność zawartość tablicy @p monos.
- * @param[in] count : liczba jednomianów
- * @param[in] monos : tablica jednomianów
- * @return wielomian będący sumą jednomianów
- */
-Poly PolyAddMonos(size_t count, const Mono monos[]);
+static inline void MonosSort(Mono monos[], size_t min, size_t max){
+  if(min < max){
+    size_t mid = min;
+    for(size_t i = min; i < max; i++){
+      if((monos[i]).exp < (monos[max]).exp){
+        MonosSwap(&(monos[mid]), &(monos[i]));
+        i++;
+      }
+    }
+    MonosSwap(&(monos[mid + 1]), &(monos[max]));
+    MonosSort(monos, min, mid);
+    MonosSort(monos, mid + 2, max);
+  }
+}
+
+Poly PolyAddMonos(size_t count, const Mono monos[]){
+  Mono monos2[count];
+  for(size_t i = 0; i < count; i++){
+    monos2[i] = MonoClone(&(monos[i]));
+  }
+  MonosSort(monos2, 0, count - 1); //tutaj to coś inaczej musi być bo monos jest const
+  Poly sum;
+  size_t imonos = 0, isum = 0;
+  sum.arr = malloc(count * sizeof(Mono));
+  while(imonos < count){
+
+  }
+  sum.arr = realloc(sum.arr, isum * sizeof(struct Mono));
+  sum.size = isum;
+  return sum;
+}
 
 /**
  * Mnoży dwa wielomiany.
@@ -119,12 +155,26 @@ poly_exp_t PolyDegBy(const Poly *p, size_t var_idx);
 poly_exp_t PolyDeg(const Poly *p);
 
 /**
- * Sprawdza równość dwóch wielomianów.
- * @param[in] p : wielomian @f$p@f$
- * @param[in] q : wielomian @f$q@f$
- * @return @f$p = q@f$
+ * Sprawdza równość dwóch jednomianów.
+ * @param[in] m : jednomian @f$m@f$
+ * @param[in] n : jednomian @f$n@f$
+ * @return @f$m = n@f$
  */
-bool PolyIsEq(const Poly *p, const Poly *q);
+bool MonoIsEq(const Mono *m, const Mono *n){
+  return (m->exp == n->exp && PolyIsEq(&(m->p), &(n->p)));
+}
+
+bool PolyIsEq(const Poly *p, const Poly *q){
+  if(!(p->arr) && !(q->arr) && p->coeff == q->coeff) return true;
+  if(p->arr && q->arr && p->size == q->size){
+    for(size_t i = 0; i < p->size; i++){
+      if(!MonoIsEq(&((p->arr)[i]),&((q->arr)[i])))
+        return false;
+    }
+    return true;
+  }
+  return false;
+}
 
 /**
  * Wylicza wartość wielomianu w punkcie @p x.
