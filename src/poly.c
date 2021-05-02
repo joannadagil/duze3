c01ec3e1f071516daf94533b3a0b24a4ee9f841e
JoannaDagil <jd417531@students.mimuw.edu.pl>
2021-05-02 05:51:25 +0200

    fighting valgrind

--- src/poly.c
+++ src/poly.c
@@ -170,7 +170,7 @@ Poly PolyAddMonos(size_t count, const Mono monos[]){
   //uwolnić tablicę monos2 ?i monos z tablicy monos?
   for(size_t i = 0; i < count; i++){
     MonoDestroy(&(monos2[i]));
-    //MonoDestroy(&(monos[i]));
+    MonoDestroy(&(monos[i]));
   }
   sum = UnproperPoly(&sum);
   return sum;
