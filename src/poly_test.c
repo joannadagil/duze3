c54ea7b5088fa58f99f03595621c02c6953688d7
JoannaDagil <jd417531@students.mimuw.edu.pl>
2021-08-31 00:05:39 +0200

    poly_test w maku

--- src/poly_test.c
+++ src/poly_test.c
@@ -4201,7 +4201,7 @@ static const test_list_t test_list[] = {
   //TEST(MulTest2),             //24
   TEST(AddTest1),             //25
   //TEST(AddTest2),             //26
-  TEST(SubTest1),             //27
+  //TEST(SubTest1),             //27
   TEST(SubTest2),             //28
   TEST(ArithmeticGroup),      //29
   TEST(IsEqTest),             //30
